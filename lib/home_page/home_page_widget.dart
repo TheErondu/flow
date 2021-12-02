ba2e5d503429976ca5ca6837c443cc199d2e2b67
unknown <erone007@gmail.com>
2021-12-02 11:08:24 +0100

    cleaned homepage

--- lib/home_page/home_page_widget.dart
+++ lib/home_page/home_page_widget.dart
@@ -1,12 +1,9 @@
 import 'package:brave/widgets/drawer_widget.dart';
 
 import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
-import '../login/login_widget.dart';
-import '../main.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:google_fonts/google_fonts.dart';
+ 
 
 class HomePageWidget extends StatefulWidget {
   HomePageWidget({Key key}) : super(key: key);
